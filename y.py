8fa12a82d7443706b703426ad9c1282f431e15ad
pranavpisal90-code <pranavpisal90@gmail.com>
2026-06-29 16:39:45 +0530

    Delete y.py

--- y.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df=pd.read_csv("churn.csv")
-
-df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
-print(df.isnull().sum())
